95b3746c8f690ea8ecc9efb9098bf527b1004210
laumack <lauren.a.mcintyre@gmail.com>
2023-09-12 12:25:53 +0100

    Game grid created

--- noughts-and-crosses/src/components/Game.jsx
+++ noughts-and-crosses/src/components/Game.jsx
@@ -1,11 +1,31 @@
 import "../styles.css";
 
+function Square({ value }) {
+  return (
+    <button id="square" type="button">
+      {value}
+    </button>
+  );
+}
+
 export default function Game() {
   return (
     <main>
-        <button id="square" type="button">
-          X
-        </button>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </main>
   );
 }
