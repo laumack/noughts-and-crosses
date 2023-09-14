a586eacb53799349a64026fa541068931d8bdaee
laumack <lauren.a.mcintyre@gmail.com>
2023-09-14 10:59:26 +0100

    Seperate helper funtions into utils file

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { calculateWinner } from "../utils.js"
 import "../styles.css";
 
 function Square({ value, onSquareClick }) {
@@ -57,22 +58,3 @@ export default function Game() {
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
